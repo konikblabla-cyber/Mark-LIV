0465633cf6ab0b401f1153db3b2845018e9a24e3
konikblabla-cyber <konikblabla@gmail.com>
2026-09-24 23:17:22 +0200

    Add consolidated regression test suite

---
+++ tests/test_suite.py
@@ -0,0 +1,20 @@
+import unittest
+
+from tests.test_permissions import PermissionsTests
+from tests.test_permissions_edge import PermissionEdgeTests
+from tests.test_action_loader import ActionLoaderTests
+from tests.test_computer_control import ComputerControlTests
+
+
+def suite():
+    loader = unittest.TestLoader()
+    return unittest.TestSuite([
+        loader.loadTestsFromTestCase(PermissionsTests),
+        loader.loadTestsFromTestCase(PermissionEdgeTests),
+        loader.loadTestsFromTestCase(ActionLoaderTests),
+        loader.loadTestsFromTestCase(ComputerControlTests),
+    ])
+
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=2).run(suite())
